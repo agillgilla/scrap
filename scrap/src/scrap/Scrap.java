c4e1ec7122a939d0a319fe34171ea989c16bd52c
Arjun Gill <agillgilla@gmail.com>
2019-01-21 21:15:32 -0800

    Added preliminary recording files

--- scrap/src/scrap/Scrap.java
+++ scrap/src/scrap/Scrap.java
@@ -6,10 +6,23 @@
 
 package scrap;
 
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 /**
  *
- * @author ArjunDesktop
+ * @author Arjun
  */
 public class Scrap {
-    
+
+    public static void main(String[] args) throws Exception {
+        ScreenRecorder.record();
+    }
 }
